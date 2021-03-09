a659701d134d2fa27603bcac72f017cf8e445459
shnippi <janjosef.schnyder@bluewin.ch>
2021-03-09 14:15:05 +0100

    added cuda conditionals

--- MNIST.py
+++ MNIST.py
@@ -7,19 +7,22 @@ from torch import optim
 from torchvision import datasets, transforms
 from torch.utils.data import random_split, DataLoader
 
-train_data = datasets.MNIST('data',train=True, download=True, transform=transforms.ToTensor())
-train, val = random_split(train_data, [55000,5000])
+train_data = datasets.MNIST('data', train=True, download=True, transform=transforms.ToTensor())
+train, val = random_split(train_data, [55000, 5000])
 train_loader = DataLoader(train, batch_size=32)
 val_loader = DataLoader(val, batch_size=32)
 
-model = nn.Sequential(nn.Linear(28*28,64),
+model = nn.Sequential(nn.Linear(28 * 28, 64),
                       nn.ReLU(),
-                      nn.Linear(64,64),
+                      nn.Linear(64, 64),
                       nn.ReLU(),
-                      nn.Linear(64,10)
-                      ).cuda()
+                      nn.Linear(64, 10)
+                      )
 
-optimiser = optim.SGD(model.parameters(), lr = 1e-2)
+if torch.cuda.is_available():
+    model = model.cuda()
+
+optimiser = optim.SGD(model.parameters(), lr=1e-2)
 
 loss = nn.CrossEntropyLoss()
 
@@ -28,12 +31,14 @@ nb_epochs = 5
 for epoch in range(nb_epochs):
     losses = list()
     for batch in train_loader:
-        x,y = batch
+        x, y = batch
         b = x.size(0)
-        x = x.view(b,-1).cuda()
+        x = x.view(b, -1)
+        if torch.cuda.is_available():
+            x = x.cuda()
 
-        l = model(x) #logit
-        J = loss(l,y.cuda())
+        l = model(x)  # logit
+        J = loss(l, y.cuda() if torch.cuda.is_available() else y)
 
         model.zero_grad()
 
@@ -43,18 +48,20 @@ for epoch in range(nb_epochs):
 
         losses.append(J.item())
 
-    print(f'Epoch {epoch+1}, train loss: {torch.tensor(losses).mean():.2f}')
+    print(f'Epoch {epoch + 1}, train loss: {torch.tensor(losses).mean():.2f}')
 
     losses = list()
     for batch in val_loader:
         x, y = batch
         b = x.size(0)
-        x = x.view(b, -1).cuda()
+        x = x.view(b, -1)
+        if torch.cuda.is_available():
+            x = x.cuda()
 
         with torch.no_grad():
             l = model(x)  # logit
 
-        J = loss(l, y.cuda())
+        J = loss(l, y.cuda() if torch.cuda.is_available() else y)
 
         losses.append(J.item())
 
