126ca4829813dff920b44982874998fc74d7b275
shnippi <janjosef.schnyder@bluewin.ch>
2021-04-20 14:36:19 +0200

    minor changes to loss

--- loss.py
+++ loss.py
@@ -14,31 +14,34 @@ class entropic_openset_loss():
         # logit_values --> tensor with #batchsize samples, per sample 10 values with logits for each class.
         # target f.e. tensor([0, 4, 1, 9, 2]) with len = batchsize
 
-        # print(logit_values)
-        # print(target)
-
         catagorical_targets = torch.zeros(logit_values.shape).to(
             self.device)  # tensor with size (batchsize, #classes), all logits to 0
         known_indexes = target != -1  # list of bools for the known classes
         unknown_indexes = ~known_indexes  # list of bools for the unknown classes
-        # print(known_indexes)
-        # print(unknown_indexes)
         catagorical_targets[known_indexes, :] = self.eye[
             target[known_indexes]]  # puts the logits to 1 at the correct index (class) for each known sample
-        # print(catagorical_targets)
         catagorical_targets[unknown_indexes, :] = self.ones.expand(
             # puts 1/#classes (0.1) for every logit(max entropy)
             (torch.sum(unknown_indexes).item(), self.num_of_classes)) * self.unknowns_multiplier
-        # print(catagorical_targets)
 
         log_values = F.log_softmax(logit_values, dim=1)  # EOS --> -log(Softmax(x))
-        # print(log_values)
         negative_log_values = -1 * log_values
         loss = negative_log_values * catagorical_targets # puts the -log-values at index for each sample (rest is 0)
-        # print(loss)
         # why is there a mean here? --> doesnt matter, leave it. just pump up learning rate
         sample_loss = torch.mean(loss, dim=1)
         # sample_loss = torch.max(loss, dim=1).values
+        # print(logit_values)
+        # print(target)
+        # print(known_indexes)
+        # print(unknown_indexes)
+        # print(catagorical_targets)
+        # print(log_values)
+        # print(loss)
+        # print(sample_loss)
+        for i in range(len(sample_loss)):
+            if known_indexes[i]:
+                sample_loss[i] *= 10
+
         # print(sample_loss)
         if sample_weights is not None:
             sample_loss = sample_loss * sample_weights
